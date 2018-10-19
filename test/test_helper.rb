9b87099deacd16e73fa13851d39d6f9b08b8319a
Magnus Hult <magnus@magnushult.se>
2018-10-19 14:52:31 +0200

    Add simplecov and codecov

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,16 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'mail_assertions'
 
+require 'simplecov'
+
+SimpleCov.start
+
+if ENV['CI']
+  # Upload code coverage reports on CI
+  require 'codecov'
+  SimpleCov.formatter = SimpleCov::Formatter::Codecov
+end
+
+require 'mail_assertions'
 require 'minitest/autorun'
 
 ActionMailer::Base.delivery_method = :test
